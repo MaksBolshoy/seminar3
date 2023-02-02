a9b1b7ac52ddbcca72adbc10a79c137b00a0481c
MaksBolshoy <118996271+MaksBolshoy@users.noreply.github.com>
2023-02-02 18:37:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Контрольная работа
+// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] array = new string[4] {"kost", "dom", "son", "tort"};
+string[] runt = new string[array.Length];
+void DostArrayLog(string[] array, string[] runt)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        runt [count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+DostArrayLog(array, runt);
+PrintArray(runt);
